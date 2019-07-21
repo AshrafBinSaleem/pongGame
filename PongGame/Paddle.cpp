85408af26367831609c66a4b1b83b6cb1c0522d8
AshrafSaleem <ses160001@student.iumw.edu.my>
2019-07-21 10:04:04 +0800

    Added Paddle Movement And Optimized Paddle Class

--- PongGame/Paddle.cpp
+++ PongGame/Paddle.cpp
@@ -4,9 +4,33 @@
 
 Paddle::Paddle()
 {
+	x = 0;
+	y = 0;
+	size = 1;
+	name = "paddle";
+	pattern = '|';
+}
+
+Paddle::Paddle(int _x, int _y) : Paddle()
+{
+	x = _x;
+	y = _y;
 }
 
 
 Paddle::~Paddle()
 {
+}
+
+std::string Paddle::displayCords() {
+	std::stringstream tmp;
+	tmp << "( " << x << " , " << y << " )" << std::endl;;
+	return tmp.str();
+}
+
+void Paddle::moveUp() {
+	y--;
+}
+void Paddle::moveDown() {
+	y++;
 }
--- PongGame/Paddle.h
+++ PongGame/Paddle.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <iostream>
+#include <sstream>
+
 class Paddle
 {
 	//Position (X,Y)
@@ -12,14 +14,27 @@ class Paddle
 	//Pattern as in design of paddle
 	char pattern;
 public:
-	Paddle() 
-	{
-		x = 0;
-		y = 0;
-		size = 0;
-		name = "test pong";
-		pattern = 'U';
-	}
+	Paddle();
+	Paddle(int x, int y);
 	~Paddle();
+
+	int getX() { return x; }
+	int getY() { return y; }
+	int getSize() { return size; }
+	std::string getName() { return name; }
+	char getPattern() { return pattern; }
+
+	std::string displayCords();
+
+	void moveUp();
+	void moveDown();
+
+	void setX(int _x) { x = _x; }
+	void setY(int _y) { y = _y; }
+	void setSize(int _size) { size = _size; }
+	void setName(std::string _name) { name = _name; }
+	void setPattern(char _pattern) { pattern = _pattern; }
+
+
 };
 
